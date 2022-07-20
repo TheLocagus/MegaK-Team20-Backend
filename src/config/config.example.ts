23e0cfd5190454fc571de107c42d99e4bc5e39a5
Adam Wojnarowski <adam.piotr.wojnarowski@gmail.com>
2022-07-20 10:03:23 +0200

    Add typeorm and mysql packages. Do the config in nest to work with database both in synchronized mode and not.

---
+++ src/config/config.example.ts
@@ -0,0 +1,10 @@
+export const config = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 8889,
+  username: 'root',
+  password: 'root',
+  database: 'megak_bonus',
+  logging: true,
+  synchronize: true,
+};
