0822c9eb97e3a35074b0b1d65f414520c1f8cc5b
Adam Wojnarowski <adam.piotr.wojnarowski@gmail.com>
2022-07-30 12:39:52 +0200

    made auth module stuctures

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { DatabaseModule } from './database/database.module';
 import { AdminModule } from './admin/admin.module';
 import { MailModule } from './mail/mail.module';
 import { RecruiterModule } from './recruiter/recruiter.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, AdminModule, MailModule, RecruiterModule],
+  imports: [DatabaseModule, AdminModule, MailModule, RecruiterModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
