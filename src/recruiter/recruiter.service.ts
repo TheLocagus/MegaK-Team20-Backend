bd7124c854ab43022df75f530e6194410e3834d3
arakeee <arake12345@gmail.com>
2022-08-11 14:49:55 +0200

    Update recruiter.service.ts

--- src/recruiter/recruiter.service.ts
+++ src/recruiter/recruiter.service.ts
@@ -29,7 +29,6 @@ export class RecruiterService {
       .leftJoinAndSelect('student.studentImport', 'studentImport')
       .where('studentImport.isActive = :isActive', { isActive: true })
       .andWhere('student.status = :status', { status: 'active' })
-      .orderBy('student.lastName', 'ASC')
         .skip(maxPerPage * (currentPage - 1))
         .take(maxPerPage)
       .getMany()
