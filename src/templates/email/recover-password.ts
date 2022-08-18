471ea0a8efc5c0036c84d8626dc42965bbb11acd
TheLocagus <locagus@o2.pl>
2022-08-19 01:00:06 +0200

    Uncommented recoverPassword before updating files on production.

--- src/templates/email/recover-password.ts
+++ src/templates/email/recover-password.ts
@@ -1,15 +1,15 @@
-export function recoverPassword(userId: string, token: string) {
-  return `
-  <h1>Witaj,</h1>
-  <p>Kliknij w poniższy link aby odzyskać hasło: </p>
-  <a href="http://localhost:3000/check-user/${userId}/${token}">Link</a>
-  `;
-}
-
 // export function recoverPassword(userId: string, token: string) {
 //   return `
 //   <h1>Witaj,</h1>
 //   <p>Kliknij w poniższy link aby odzyskać hasło: </p>
-//   <a href="https://megakheadhunters-team20.networkmanager.pl/check-user/${userId}/${token}">Link</a>
+//   <a href="http://localhost:3000/check-user/${userId}/${token}">Link</a>
 //   `;
 // }
+
+export function recoverPassword(userId: string, token: string) {
+  return `
+  <h1>Witaj,</h1>
+  <p>Kliknij w poniższy link aby zmienić hasło: </p>
+  <a href="https://megakheadhunters-team20.networkmanager.pl/check-user/${userId}/${token}">Link</a>
+  `;
+}
